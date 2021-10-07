a682d7916d259e074d8009b1f5e19f92f96f5b0f
stelfOriginal <lstelf32@gmail.com>
2021-10-07 18:27:50 +0200

    primer comentario

---
+++ readme.txt
@@ -0,0 +1,20 @@
+En mi caso he usado xamp como servidor para ejecutar y visiualizarlo,en este caso es tan facil como instalarlo https://www.apachefriends.org/es/index.html seguir los pasos
+y en el panel al instalarse pulsar el boton start de apache.
+
+Lo he intentado hacer muy intuitivo y sin que exista manera de perderse o deriar por equivocacion en otros apartados o que sea tedioso,como se pidio lo he echo simple.
+
+En el archivo login.php en sus primeras lineas se tiene especificado el administrador y contraseña con el que entrar,he utilizado la palabra "admin"en ambos casos para que
+ no sea ambigua o dificil de acordarse.Si se introduce otros valores la pagina pasa por un condicional que te devuelve un enlaze de vuelta al formulario y un mensaje de error.
+ En caso de que se verifiquen los datos te lleva a lo que seria el perfil del usuario,segun los datos que se me dieron lo he iamginado como si fuera un perfil de un apostador 
+y se podrian detallar sus apuestas con peticiones,datos,un historial(lo cual no se ha incluido,simplemente me ayuda a comprender mejor la peticion).
+Al entrar te da un mensaje de si quieres ver el codigo y dos opciones,verlo o cerrar session.
+Al clicar en ver el codigo la funcion runMyFunction se ejecuta y da una cadena de 7 numeros al azar entre 0 y 9 que sera el codigo facilitado.
+Se puede seguir dando a ver mi codigo por si el usuario quiere alguna otra variable distinta.
+
+Una vez conforme con el numero seleccionado se pulsa canjear codigo,el cual crea un popup inevitable que notifica la aceptacion del codigo y te da la opcion de cerrar sesion(ya que 
+una vez canjeado el codigo dudo que se quiera seguir alli)
+
+
+Si tiene cualquier duda puede comunicarse conmigo por el correo de lstelf32@gmail.com,espero que cumpla con sus espectativas.
+
+
---
+++ secreta.php
@@ -0,0 +1,62 @@
+<html>
+    <link rel="stylesheet" href="css/style.css">
+<?php
+# Si no entiendes el código, primero mira a login.php
+
+# Iniciar sesión para usar $_SESSION
+session_start();
+
+# Y ahora leer si NO hay algo llamado usuario en la sesión,
+# usando empty (vacío, ¿está vacío?)
+# Recomiendo: https://parzibyte.me/blog/2018/08/09/isset-vs-empty-en-php/
+if (empty($_SESSION["usuario"])) {
+    # Lo redireccionamos al formulario de inicio de sesión
+    header("Location: formulario.html");
+    # Y salimos del script
+    exit();
+}
+
+# No hace falta un else, pues si el usuario no se loguea, todo lo de abajo no se ejecuta
+echo "Quieres ver tus codigo?";
+echo "<br>";
+    #creamos una funcion que nos proporcione de manera aleatoria los codigos 
+
+    function runMyFunction() {
+        $entrada = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+        $claves_aleatorias = array_rand($entrada, 7);
+        echo $entrada[$claves_aleatorias[0]] . "\n";
+        echo $entrada[$claves_aleatorias[1]] . "\n";
+        echo $entrada[$claves_aleatorias[2]] . "\n";
+        echo $entrada[$claves_aleatorias[3]] . "\n";
+        echo $entrada[$claves_aleatorias[4]] . "\n";
+        echo $entrada[$claves_aleatorias[5]] . "\n";
+        echo $entrada[$claves_aleatorias[6]] . "\n";;
+    }
+
+    #Repetimos tantas vezes como codigos se den en la pagina,en este caso 3 por persona
+    if (isset($_GET['hello'])) {
+        echo "este es tu codigo:";
+        runMyFunction();
+        echo "<br>";
+        
+        if (isset($_POST['aceptado'])) {
+             echo "canjeado correctamente";
+             } 
+     echo "<p><a href='#popup'>canjear codigo</a></p>";
+    }
+    echo "<br>";
+?>
+<div id="popup" class="overlay">
+            <div id="popupBody">
+                <h2>Canjeado correctamente</h2>
+                <a id="cerrar" type="hidden" href="#">&times;</a>
+                <div class="popupContent">
+                    <p>Que tenga un buen dia</p>
+                </div>
+                <a href="logout.php">Cerrar sesión</a>
+            </div>
+        </div>
+<a href='secreta.php?hello=true'>Ver mi codigo</a><br>
+<!-- Y aprovechando, le indicamos al usuario un enlace para salir-->
+<a href="logout.php">Cerrar sesión</a>
+</html>
