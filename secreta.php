4ecb9da0953775e16b4a3b1e7051d2e7f432680f
stelfOriginal <49567847+stelfOriginal@users.noreply.github.com>
2021-10-09 02:08:49 +0200

    Update secreta.php
    arreglar comentarios mal definidos

--- secreta.php
+++ secreta.php
@@ -1,14 +1,12 @@
 <html>
     <link rel="stylesheet" href="css/style.css">
 <?php
-# Si no entiendes el código, primero mira a login.php
 
 # Iniciar sesión para usar $_SESSION
 session_start();
 
 # Y ahora leer si NO hay algo llamado usuario en la sesión,
 # usando empty (vacío, ¿está vacío?)
-# Recomiendo: https://parzibyte.me/blog/2018/08/09/isset-vs-empty-en-php/
 if (empty($_SESSION["usuario"])) {
     # Lo redireccionamos al formulario de inicio de sesión
     header("Location: formulario.html");
